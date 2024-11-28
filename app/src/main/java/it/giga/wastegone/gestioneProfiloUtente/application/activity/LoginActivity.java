2f0a2eee407cbbe345e8563cdb52d4e1836e3d84
Simon <s.carbone24@studenti.unisa.it>
2024-11-28 14:45:19 +0100

    Feat: Aggiunte funzionalità e commenti alla classe LoginActivity

--- app/src/main/java/it/giga/wastegone/gestioneProfiloUtente/application/activity/LoginActivity.java
+++ app/src/main/java/it/giga/wastegone/gestioneProfiloUtente/application/activity/LoginActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,9 +17,9 @@ import androidx.core.view.WindowInsetsCompat;
 import it.giga.wastegone.R;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText emailText, passwordText;
-    private Button loginButton;
-    private TextView registratiView;
+    private EditText etMail, etPassword;
+    private Button btnLogin;
+    private TextView tvRegistrati;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,43 +28,53 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.login_layout);
 
-        emailText = findViewById(R.id.textemail);
-        passwordText = findViewById(R.id.textpassword);
-        loginButton = findViewById(R.id.login);
-        registratiView = findViewById(R.id.registrati);
+        etMail = findViewById(R.id.textemail);
+        etPassword = findViewById(R.id.textpassword);
+        btnLogin = findViewById(R.id.loginbutton);
+        tvRegistrati = findViewById(R.id.registrati);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        //Quando viene premuto il bottone accedi vengono inviate  le informazioni del form al metodo che permette il login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                String email = emailText.getText().toString();
-                String password = passwordText.getText().toString();
+                String email = etMail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Devi compilare entrambi i campi!", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Metodo da implementare se i campi sono validi
+                    onLoginClicked(email, password);
+                }
 
                 // Metodo da implementare
                 onLoginClicked(email, password);
             }
         });
-
-        registratiView.setOnClickListener(new View.OnClickListener() {
+        // Quando viene premuto il testo "Registrati" viene aperta la schermata di registrazione
+        tvRegistrati.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Metodo da implementare
-                onRegistratiClicked();
+                ontvRegistratiClicked();
             }
         });
 
     }
 
-    private void onRegistratiClicked() {
+    //Metodo usato per aprire la schermata di registrazione
+    private void ontvRegistratiClicked() {
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
 
+    //Metodo usato per effettuare il login
     private void onLoginClicked (String email, String password){
+
         }
 
 
