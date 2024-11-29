7b8a54ae291e53ced2fab8498487fc94b761ee9f
Elisa Picilli <e.picilli@studenti.unisa.it>
2024-11-29 17:08:46 +0100

    Feat: Aggiunta classe NotificationLogic con relativi metodi.

--- app/src/main/java/it/giga/wastegone/gestioneProfiloUtente/application/logic/NotificationLogic.java
+++ app/src/main/java/it/giga/wastegone/gestioneProfiloUtente/application/logic/NotificationLogic.java
@@ -1,6 +1,5 @@
 package it.giga.wastegone.gestioneProfiloUtente.application.logic;
 
-
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
@@ -11,17 +10,28 @@ import androidx.core.app.NotificationCompat;
 
 import it.giga.wastegone.R;
 
-public class NotificationReceiver extends BroadcastReceiver {
+/**
+ * Classe che gestisce la logica delle notifiche.
+ */
+public class NotificationLogic extends BroadcastReceiver {
 
+    /**
+     * Metodo chiamato quando viene ricevuto un broadcast.
+     *
+     * @param context Il contesto dell'applicazione.
+     * @param intent  L'intent che ha attivato il broadcast.
+     */
     @Override
     public void onReceive(Context context, Intent intent) {
+        // Recupera il messaggio dagli extra dell'intent
         String message = intent.getStringExtra("message");
 
+        // Ottiene il servizio di NotificationManager
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         String channelId = "notifications_channel";
 
-        // Crea il canale per Android Oreo e versioni successive
+        // Crea un canale di notifica per Android O e versioni successive
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(
                     channelId,
@@ -33,12 +43,13 @@ public class NotificationReceiver extends BroadcastReceiver {
 
         // Crea e mostra la notifica
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
-                .setSmallIcon(R.drawable.logodark)
-                .setContentTitle("WasteGone ti ricorda:")
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setAutoCancel(true);
+                .setSmallIcon(R.drawable.logodark) // Imposta l'icona della notifica
+                .setContentTitle("WasteGone ti ricorda:") // Imposta il titolo della notifica
+                .setContentText(message) // Imposta il testo della notifica
+                .setPriority(NotificationCompat.PRIORITY_HIGH) // Imposta la priorità della notifica
+                .setAutoCancel(true); // Rimuove automaticamente la notifica quando viene cliccata
 
+        // Verifica che il messaggio non sia nullo e mostra la notifica
         assert message != null;
         notificationManager.notify(message.hashCode(), builder.build());
     }
