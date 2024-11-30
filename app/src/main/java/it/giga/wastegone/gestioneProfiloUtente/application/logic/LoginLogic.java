7a236728b1eaacab53862b649d1ea557f67d9e5d
alessiagatto <alessiagatto.3@gmail.com>
2024-11-30 11:57:57 +0100

    Fix: Aggiunta commenti javadoc

--- app/src/main/java/it/giga/wastegone/gestioneProfiloUtente/application/logic/LoginLogic.java
+++ app/src/main/java/it/giga/wastegone/gestioneProfiloUtente/application/logic/LoginLogic.java
@@ -5,7 +5,15 @@ import java.util.Map;
 
 public class LoginLogic {
 
+
     private Map<String, String> database = new HashMap<>();
+
+    /**
+     * Verifica se l'email fornita è presente nel database.
+     *
+     * @param email l'email da verificare
+     * @return true se l'email è nel database, false altrimenti
+     */
         public boolean isEmailInDatabase(String email) {
             return database.containsKey(email);
         }
