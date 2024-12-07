0ecce0f98c7479ebab9bd89a5ce395ad2113a407
alessiagatto <alessiagatto.3@gmail.com>
2024-12-07 15:08:58 +0100

    Fix: Check Style applicato

--- app/src/main/java/it/giga/wastegone/gestioneEventiSensibilizzazione/application/logic/PrenotazioneLogic.java
+++ app/src/main/java/it/giga/wastegone/gestioneEventiSensibilizzazione/application/logic/PrenotazioneLogic.java
@@ -1,45 +1,47 @@
 package it.giga.wastegone.gestioneEventiSensibilizzazione.application.logic;
 
-import static com.google.android.gms.tasks.Task.*;
-import static com.google.android.gms.tasks.Tasks.forResult;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
-import com.google.firebase.Timestamp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import it.giga.wastegone.gestioneEventiSensibilizzazione.storage.entity.Prenotazione;
-import it.giga.wastegone.gestioneProfiloUtente.storage.dataAccess.FirebaseUserDAO;
-import it.giga.wastegone.gestioneProfiloUtente.storage.entity.User;
 
+/**
+ * Classe che gestisce la logica di salvataggio di una prenotazione.
+ */
 public class PrenotazioneLogic {
-    /**
-     * Gestisce la logica di salvataggio di una prenotazione.
-     *
-     * @param userID      l'ID dell'utente che effettua la prenotazione
-     * @param description la descrizione dell'evento
-     * @param date        la data dell'evento
+  /**
+  * Gestisce la logica di salvataggio di una prenotazione.
+  *
+  * @param userId      l'ID dell'utente che effettua la prenotazione
+  * @param description la descrizione dell'evento
+  * @param date        la data dell'evento
 
-     * @return un Task che rappresenta l'operazione asincrona di salvataggio della prenotazione
-     */
-    public Task<Void> handleSubmit(String userID, String description, String date, String hour) {
+  * @return un Task che rappresenta l'operazione asincrona di salvataggio della prenotazione
+  */
+  public Task<Void> handleSubmit(String userId, String description, String date, String hour) {
 
-        // Crea un'istanza della prenotazione
-        Prenotazione prenotazione = new Prenotazione(userID, description, date, hour);
+    // Crea un'istanza della prenotazione
+    Prenotazione prenotazione = new Prenotazione(userId, description, date, hour);
 
-        // Salva la prenotazione nel database
-        return savePrenotazione(prenotazione);
+    // Salva la prenotazione nel database
+    return savePrenotazione(prenotazione);
 
-    }
+  }
 
-    public Task<Void> savePrenotazione(Prenotazione prenotazione) {
-        return FirebaseFirestore.getInstance().collection("Prenotazioni").add(prenotazione).continueWithTask(task -> {
-            if (task.isSuccessful()) {
+  /**
+  * Salva una prenotazione nel database.
+  *
+  * @param prenotazione la prenotazione da salvare
+  * @return un Task che rappresenta l'operazione asincrona di salvataggio della prenotazione
+  */
+  public Task<Void> savePrenotazione(Prenotazione prenotazione) {
+    return FirebaseFirestore.getInstance().collection("Prenotazioni")
+            .add(prenotazione).continueWithTask(task -> {
+              if (task.isSuccessful()) {
                 return Tasks.forResult(null);
-            } else {
+              } else {
                 throw task.getException();
-            }
-        });
-    }
+              }
+            });
+  }
 }
