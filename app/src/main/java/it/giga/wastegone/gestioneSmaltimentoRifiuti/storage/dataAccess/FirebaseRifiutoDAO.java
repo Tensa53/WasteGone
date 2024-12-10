a97473f41fc884f358dd22536c49da47f88f3fc7
Francesco Laudano <f.laudano3@studenti.unisa.it>
2024-12-10 11:00:18 +0100

    Fix: Aggiunti commenti JavaDoc a FirebaseRifiutoDAO

--- app/src/main/java/it/giga/wastegone/gestioneSmaltimentoRifiuti/storage/dataAccess/FirebaseRifiutoDAO.java
+++ app/src/main/java/it/giga/wastegone/gestioneSmaltimentoRifiuti/storage/dataAccess/FirebaseRifiutoDAO.java
@@ -10,32 +10,66 @@ import java.time.DayOfWeek;
 
 import it.giga.wastegone.gestioneSmaltimentoRifiuti.storage.entity.Rifiuto;
 
+/**
+ * Questa classe rappresenta un oggetto per l'interazione con i dati dei rifiuti memorizzati
+ * nel database Firebase Firestore.
+ */
 public class FirebaseRifiutoDAO {
     private static final String TABLE_NAME = "Rifiuti";
 
     FirebaseFirestore db;
 
 
+    /**
+     * Costruttore della classe.
+     * Inizializza l'istanza del database.
+     */
     public FirebaseRifiutoDAO() {
         db = FirebaseFirestore.getInstance();
     }
 
 
+    /**
+     * Recupera il documento del rifiuto tramite id.
+     *
+     * @param id, id del documento desiderato.
+     * @return Task<DocumentSnapshot> rappresenta il risultato dell'operazione.
+     */
     public Task<DocumentSnapshot> doRetrieveRifiutoById(String id) {
         return db.collection(TABLE_NAME).document(id).get();
     }
 
 
+    /**
+     * Salva il rifiuto nel database.
+     *
+     * @param rifiuto, rifiuto da salvare.
+     * @param id, id del documento in cui salvare il rifiuto.
+     * @return Task<Void> rappresenta il risultato dell'operazione.
+     */
     public Task<Void> doSaveRifiuto(Rifiuto rifiuto, String id) {
         return db.collection(TABLE_NAME).document(id).set(rifiuto);
     }
 
 
+    /**
+     * Rimuove il rifiuto dal database.
+     *
+     * @param id, id del documento desiderato.
+     * @return Task<Void> rappresenta il risultato dell'operazione.
+     */
     public Task<Void> doDeleteRifiuto(String id) {
         return db.collection(TABLE_NAME).document(id).delete();
     }
 
 
+    /**
+     * Aggiorna i dati del rifiuto nel database.
+     *
+     * @param rifiuto, rifiuto aggiornato
+     * @param id, id del documento da modificare.
+     * @return Task<Void> rappresenta il risultato dell'operazione.
+     */
     public Task<Void> doUpdateRifiuto(Rifiuto rifiuto, String id) {
         DocumentReference doc = db.collection(TABLE_NAME).document(id);
 
@@ -43,34 +77,67 @@ public class FirebaseRifiutoDAO {
     }
 
 
+    /**
+     * Recupera tutti i documenti dei rifiuti memorizzati nel database.
+     *
+     * @return Task<QuerySnapshot> un oggetto Task che contiene i risultati della query.
+     */
     public Task<QuerySnapshot> doRetrieveAllRifiuti() {
         return db.collection(TABLE_NAME).get();
     }
 
 
+    /**
+     * Recupera tutti i documenti dei rifiuti speciali memorizzati nel database.
+     *
+     * @return Task<QuerySnapshot> un oggetto Task che contiene i risultati della query.
+     */
     public Task<QuerySnapshot> doRetrieveAllRifiutiSpeciali() {
         return db.collection(TABLE_NAME).
                whereEqualTo("tipo", Rifiuto.Tipo.SPECIALE).get();
     }
 
 
+    /**
+     * Recupera tutti i documenti dei rifiuti urbani memorizzati nel database.
+     *
+     * @return Task<QuerySnapshot> un oggetto Task che contiene i risultati della query.
+     */
     public Task<QuerySnapshot> doRetrieveAllRifiutiUrbani() {
         return db.collection(TABLE_NAME).
                 whereEqualTo("tipo", Rifiuto.Tipo.URBANO).get();
     }
 
 
+    /**
+     * Recupera tutti i documenti dei rifiuti che si conferiscono al giorno specificato.
+     *
+     * @param dayOfWeek giorno di conferimento desiderato.
+     * @return Task<QuerySnapshot> un oggetto Task che contiene i risultati della query.
+     */
     public Task<QuerySnapshot> doRetrieveAllRifiutiByDayOfWeek(DayOfWeek dayOfWeek) {
         return db.collection(TABLE_NAME).
                 whereEqualTo("giornoConferimento", dayOfWeek).get();
     }
 
 
+    /**
+     * Recupera tutti i documenti dei rifiuti costituiti dal materiale specificato.
+     *
+     * @param materiale materiale desiderato.
+     * @return Task<QuerySnapshot> un oggetto Task che contiene i risultati della query.
+     */
     public Task<QuerySnapshot> doRetrieveAllRifiutiByMateriale(String materiale) {
         return db.collection(TABLE_NAME).
                 whereEqualTo("materiale", materiale).get();
     }
 
+    /**
+     * Recupera tutti i documenti dei rifiuti appartenenti alla categoria specificata.
+     *
+     * @param categoria categoria desiderata.
+     * @return Task<QuerySnapshot> un oggetto Task che contiene i risultati della query.
+     */
     public Task<QuerySnapshot> doRetrieveAllRifiutiByCategoria(Rifiuto.Categoria categoria) {
         return db.collection(TABLE_NAME).
                 whereEqualTo("categoria", categoria).get();
