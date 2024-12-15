0d52329c9d3e160a20bd753d31da21c590343565
alessiagatto <alessiagatto.3@gmail.com>
2024-12-15 19:17:16 +0100

    Fix: modifica valore della variabile nome

--- app/src/test/java/it/giga/wastegone/gestioneEventiSensibilizzazione/VerificaTassa/VerificaTassaTest2.java
+++ app/src/test/java/it/giga/wastegone/gestioneEventiSensibilizzazione/VerificaTassa/VerificaTassaTest2.java
@@ -8,7 +8,7 @@ public class VerificaTassaTest2 {
 
     @Test
     public void testTassa() {
-        String nome = "Giovanniiiiiiiiiiiii";
+        String nome = "Giovanniiiiiiiiiiiiiii";
         String cognome= "Verdi";
         String numeroCarta = "1111222233334444";
         String scadenza = "04/25";
